0c18829ca91c6934213f79a9e9396e1bd00ed369
Oleksii83 <zaaytav@gmail.com>
2021-07-06 22:53:07 +0300

    Btn submit
    Подвязал кнопку поиска

--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ import galleryTamplate from '../src/template/gallery.hbs';
 import photoCardTemplate from '../src/template/photo-card.hbs';
 import searchFormTemplate from '../src/template/search-form.hbs';
 
-const API_KEY = '22354412-39f12e0c13d349d19862b3301';
-const BASE_URL = 'https://pixabay.com/api/';
+const refs = {
+  galleryContainer: document.querySelector('.js-gallery_container'),
+  searchForm: document.querySelector('.search-form'),
+  loadMoreBtn: document.querySelector('[data-action="load_more"]'),
+};
+console.log(refs.searchForm);
 
-const url = fetch(
-  `${BASE_URL}?image_type=photo&orientation=horizontal&q&page&per_page=12&key=${API_KEY}`,
-)
-  .then(r => r.json())
-  .then(console.log);
+refs.searchForm.addEventListener('submit', onSearch);
+// refs.loadMoreBtn.addEventListener('click' onLoadmore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const searchQuery = e.currentTarget.elements.query.value;
+
+  const API_KEY = '22354412-39f12e0c13d349d19862b3301';
+  const BASE_URL = 'https://pixabay.com/api/';
+
+  const url = fetch(
+    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=10&key=${API_KEY}`,
+  )
+    .then(r => r.json())
+    .then(hits => {
+      const markup = photoCardTemplate(hits);
+      refs.galleryContainer.innerHTML = markup;
+    })
+
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function rendergalleryImageItem(array) {
+  const markup = galleryTamplate(gallery);
+  refs.galleryContainer.innerHTML = markup;
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,11 +1,41 @@
-const API_KEY = 22354412-39f12e0c13d349d19862b3301
-// https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
+// export default class newApiService {
+//   constructor() {
+//     this.searchQuery = '';
+//   }
+//   fetchArticles() {
+//     const API_KEY = '22354412-39f12e0c13d349d19862b3301';
+//     const BASE_URL = 'https://pixabay.com/api/';
 
-const BASE_URL = 'https://pixabay.com/api/';
-function fetchCountries(countriesName) {
-  return fetch(`${BASE_URL}/name/${countriesName}`).then(response => {
-    return response.json();
-  });
-}
+//     const url = fetch(hits)(
+//       `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=1&per_page=10&key=${API_KEY}`,
+//     )
+//       .then(r => r.json())
+//       .then(hits => {
+//         const markup = photoCardTemplate(hits);
+//         refs.galleryContainer.innerHTML = markup;
+//       })
 
-export default { fetchCountries };
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }
+//   get query() {
+//     return this.searchQuery;
+//   }
+
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+// }
+
+// const API_KEY = 22354412-39f12e0c13d349d19862b3301
+// // https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
+
+// const BASE_URL = 'https://pixabay.com/api/';
+// function fetchCountries(countriesName) {
+//   return fetch(`${BASE_URL}/name/${countriesName}`).then(response => {
+//     return response.json();
+//   });
+// }
+
+// export default { fetchCountries };
