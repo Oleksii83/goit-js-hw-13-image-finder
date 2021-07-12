cd373b2aaf46d760d1cc73b999ef089d2d7bb6fc
Oleksii83 <zaaytav@gmail.com>
2021-07-12 21:04:45 +0300

    кнопка не работает

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
+// import API from './js/apiService';
 import galleryTamplate from '../src/template/gallery.hbs';
 import photoCardTemplate from '../src/template/photo-card.hbs';
 import searchFormTemplate from '../src/template/search-form.hbs';
@@ -11,22 +12,24 @@ const refs = {
 console.log(refs.searchForm);
 
 refs.searchForm.addEventListener('submit', onSearch);
-// refs.loadMoreBtn.addEventListener('click' onLoadmore);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+let searchQuery = '';
 
 function onSearch(e) {
   e.preventDefault();
 
-  const searchQuery = e.currentTarget.elements.query.value;
+  searchQuery = e.currentTarget.elements.query.value;
 
   const API_KEY = '22354412-39f12e0c13d349d19862b3301';
   const BASE_URL = 'https://pixabay.com/api/';
 
   const url = fetch(
-    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=10&key=${API_KEY}`,
+    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=3&key=${API_KEY}`,
   )
     .then(r => r.json())
-    .then(hits => {
-      const markup = photoCardTemplate(hits);
+    .then(data => {
+      const markup = photoCardTemplate(data);
       refs.galleryContainer.innerHTML = markup;
     })
 
@@ -38,4 +41,21 @@ function onSearch(e) {
 function rendergalleryImageItem(array) {
   const markup = galleryTamplate(gallery);
   refs.galleryContainer.innerHTML = markup;
+}
+
+function onLoadMore() {
+  const API_KEY = '22354412-39f12e0c13d349d19862b3301';
+  const BASE_URL = 'https://pixabay.com/api/';
+
+  const url = fetch(
+    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=3&key=${API_KEY}`,
+  )
+    .then(r => r.json())
+    .then(data => {
+      const markup = photoCardTemplate(data);
+      refs.galleryContainer.innerHTML = markup;
+    })
+    .then(data => {
+      this.page += 1;
+    });
 }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,3 +1,27 @@
+const API_KEY = '22354412-39f12e0c13d349d19862b3301';
+const BASE_URL = 'https://pixabay.com/api/';
+
+function mainLineData() {
+  return fetch(
+    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=3&key=${API_KEY}`,
+  ).then(response => {
+    return response.json();
+  });
+}
+export default { mainLineData };
+
+// const BASE_URL = 'https://pixabay.com/api/';
+// function fetchCountries(countriesName) {
+//   return fetch(`${BASE_URL}/name/${countriesName}`).then(response => {
+//     return response.json();
+//   });
+// }
+
+// export default { fetchCountries };
+
+// const API_KEY = 22354412-39f12e0c13d349d19862b3301
+// // https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
+
 // export default class newApiService {
 //   constructor() {
 //     this.searchQuery = '';
@@ -27,15 +51,3 @@
 //     this.searchQuery = newQuery;
 //   }
 // }
-
-// const API_KEY = 22354412-39f12e0c13d349d19862b3301
-// // https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
-
-// const BASE_URL = 'https://pixabay.com/api/';
-// function fetchCountries(countriesName) {
-//   return fetch(`${BASE_URL}/name/${countriesName}`).then(response => {
-//     return response.json();
-//   });
-// }
-
-// export default { fetchCountries };
