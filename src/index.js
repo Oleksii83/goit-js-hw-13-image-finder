0bd17c6ba2d25fd1b2a6cc27efe77a37764ddc1d
Oleksii83 <zaaytav@gmail.com>
2021-07-14 23:00:09 +0300

    button and visual
    закончил настройку кнопок и шаблона (по Репете)

--- src/index.js
+++ src/index.js
@@ -16,35 +16,30 @@ console.log('🚀 ~ file: index.js ~ line 14 ~ newsAPI', newsAPI);
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-// let searchQuery = '';
-
 function onSearch(e) {
   e.preventDefault();
 
+  clearContainer();
   newsAPI.query = e.currentTarget.elements.query.value;
-
-  newsAPI.fetchArticles();
+  newsAPI.resetPage();
+  newsAPI.fetchArticles().then(rendergalleryImageItem);
 }
 
-function rendergalleryImageItem(array) {
-  const markup = galleryTamplate(gallery);
+function rendergalleryImageItem(hits) {
+  const markup = galleryTamplate(articles);
   refs.galleryContainer.innerHTML = markup;
 }
 
 function onLoadMore() {
-  newsAPI.fetchArticles();
-  // const API_KEY = '22354412-39f12e0c13d349d19862b3301';
-  // const BASE_URL = 'https://pixabay.com/api/';
+  newsAPI.fetchArticles().then(rendergalleryImageItem);
+}
 
-  // const url = fetch(
-  //   `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=3&key=${API_KEY}`,
-  // )
-  //   .then(r => r.json())
-  //   .then(data => {
-  //     const markup = photoCardTemplate(data);
-  //     refs.galleryContainer.innerHTML = markup;
-  //   })
-  //   .then(data => {
-  //     this.page += 1;
-  //   });
+function rendergalleryImageItem(hits) {
+  refs.galleryContainer.insertAdjacentHTML(
+    'beforeend',
+    photoCardTemplate(hits),
+  );
+}
+function clearContainer() {
+  refs.galleryContainer.innerHTML = '';
 }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -9,12 +9,13 @@ export default class NesApiService {
     const API_KEY = '22354412-39f12e0c13d349d19862b3301';
     const BASE_URL = 'https://pixabay.com/api/';
 
-    const url = fetch(
-      `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&${this.page}&per_page=3&key=${API_KEY}`,
-    )
+    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=3&key=${API_KEY}`;
+    return fetch(url)
       .then(r => r.json())
       .then(data => {
-        this.page += 1;
+        this.incrementPage();
+
+        return data.hits;
       });
     // .then(data => {
     //   const markup = photoCardTemplate(data);
@@ -25,6 +26,12 @@ export default class NesApiService {
     //   console.log(error);
     // });
   }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
   get query() {
     return this.searchQuery;
   }
@@ -32,47 +39,3 @@ export default class NesApiService {
     this.searchQuery = newQuery;
   }
 }
-
-// export default { mainLineData };
-
-// const BASE_URL = 'https://pixabay.com/api/';
-// function fetchCountries(countriesName) {
-//   return fetch(`${BASE_URL}/name/${countriesName}`).then(response => {
-//     return response.json();
-//   });
-// }
-
-// export default { fetchCountries };
-
-// const API_KEY = 22354412-39f12e0c13d349d19862b3301
-// // https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
-
-// export default class newApiService {
-//   constructor() {
-//     this.searchQuery = '';
-//   }
-//   fetchArticles() {
-//     const API_KEY = '22354412-39f12e0c13d349d19862b3301';
-//     const BASE_URL = 'https://pixabay.com/api/';
-
-//     const url = fetch(hits)(
-//       `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=1&per_page=10&key=${API_KEY}`,
-//     )
-//       .then(r => r.json())
-//       .then(hits => {
-//         const markup = photoCardTemplate(hits);
-//         refs.galleryContainer.innerHTML = markup;
-//       })
-
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   }
-//   get query() {
-//     return this.searchQuery;
-//   }
-
-//   set query(newQuery) {
-//     this.searchQuery = newQuery;
-//   }
-// }
